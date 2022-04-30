114266588dfea247350e19ef24f33c7cc2179625
pavanchambhare <pavanchambhare88@gmail.com>
2022-04-30 14:51:25 +0530

    first commit

--- script.js
+++ script.js
@@ -1,39 +1,22 @@
-const text=document.getElementById("quote");
-const author=document.getElementById("author");
-const tweetButton=document.getElementById("tweet");
+var counter = document.querySelector(".counter");
+var follower = document.querySelector(".follower");
 
-const getNewQuote = async () =>
-{
-    //api for quotes
-    var url="https://type.fit/api/quotes";    
 
-    // fetch the data from api
-    const response=await fetch(url);
-    console.log(typeof response);
-    //convert response to json and store it in quotes array
-    const allQuotes = await response.json();
 
-    // Generates a random number between 0 and the length of the quotes array
-    const indx = Math.floor(Math.random()*allQuotes.length);
+let count = 1;
 
-    //Store the quote present at the randomly generated index
-    const quote=allQuotes[indx].text;
-    
-    //Store the author of the respective quote
-    const auth=allQuotes[indx].author;
 
-    if(auth==null)
-    {
-        author = "Anonymous";
-    }
- 
-    //function to dynamically display the quote and the author
-    text.innerHTML=quote;
-    author.innerHTML="~ "+auth;
 
-    //tweet the quote
-    tweetButton.href="https://twitter.com/intent/tweet?text="+quote+" ~ "+auth;
+setInterval( () => {
+ if (count < 1000){
+ count++;
+ counter.innerText = count;
 
-}
+ }
+}, 10);
 
-getNewQuote();
+setTimeout( () => {
+  follower.innerText = "Follower Complete";
+},17000 );
+
+
